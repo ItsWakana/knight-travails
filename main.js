ea6307d0742ce0575eebd26b0d67b55147332303
wakana <wakanawardrobe@gmail.com>
2022-11-09 14:58:59 +0000

    first commeents and steps for implementation

---
+++ main.js
@@ -0,0 +1,3 @@
+//function knightMoves which when provided with a set of coordinates like [0,0] and [3,3] will show all the moves taken to get to [3,3]. These would be the coordinates of our board.
+
+//create a gameboard to work with (2 dimensional array with 8 rows and 8 columns)
