d68aca103d685122b3faf248b4d6af7bebe81a87
wakana <wakanawardrobe@gmail.com>
2022-11-11 16:19:01 +0000

    basic pseudocode for search algo

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 //function knightMoves which when provided with a set of coordinates like [0,0] and [3,3] will show all the moves taken to get to [3,3]. These would be the coordinates of our board.
 
 //create a gameboard to work with (2 dimensional array with 8 rows and 8 columns)
+
+
+//search algo
+//take a starting position and an ending position.
+//for the starting position on the board, check all the positions that the knight could move to from that position (two places forward and both sides). 
