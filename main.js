d4581bf2149637557d36d5c4716b2eba4aeb521e
wakana <wakanawardrobe@gmail.com>
2022-11-18 16:06:26 +0000

    reset project

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-//function knightMoves which when provided with a set of coordinates like [0,0] and [3,3] will show all the moves taken to get to [3,3]. These would be the coordinates of our board.
+const knight = 'k';
 
-//create a gameboard to work with (2 dimensional array with 8 rows and 8 columns)
+chessboard = [
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+    ['','','','','','','',''],
+]
 
+makeGraph = (board, knight) => {
 
-//search algo
-//take a starting position and an ending position.
-//for the starting position on the board, check all the positions that the knight could move to from that position (two places forward and both sides). 
+    board[0][1] = knight;
+
+}
+
+const getMovesFromCurrentPos = (board, position) => {
+
+}
