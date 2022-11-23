6df5c61540f0df05d78adf4dea77ebc08f8042f6
wakana <wakanawardrobe@gmail.com>
2022-11-23 16:42:44 +0000

    dfs on chess board

--- main.js
+++ main.js
@@ -11,35 +11,46 @@ chessboard = [
     ['','','','','','','',''],
 ]
 
-//moves from 4,4
-
 const pairs = [[-1,-2], [-2,-1], [-2,1], [-1,2], [+1,2], [2,1], [2,-1], [1,-2]];
 
-const getMoves = (currentPos) => {
-    //from our array of coordinates, take in our current position and get all the positions of our array from the current position.
-    const arrCoordinates = [];
+// const getMoves = (currentPos) => {
+//     //from our array of coordinates, take in our current position and get all the positions of our array from the current position.
+//     const arrCoordinates = [];
 
-    for (let pair of pairs) {
-        //go through our pairs and get the correct indexes for our new position
-        const first = currentPos[0] + pair[0];
-        const second = currentPos[1] + pair[1];
-        const coordinateMove = [first,second];
+//     for (let pair of pairs) {
+//         //go through our pairs and get the correct indexes for our new position
+//         const xPosition = currentPos[0] + pair[0];
+//         const yPosition = currentPos[1] + pair[1];
+//         const coordinateMove = [xPosition, yPosition];
 
-        //write some check here to make sure the move is actually valid
-        if (coordinateMove[0] >= 0 && coordinateMove[0] < 9 && 
-            coordinateMove[1] >= 0 && coordinateMove[1] < 9) {
-            arrCoordinates.push(coordinateMove)
+//         //write some check here to make sure the move is actually valid
+//         if (coordinateMove[0] >= 0 && coordinateMove[0] < 8 && 
+//             coordinateMove[1] >= 0 && coordinateMove[1] < 8) {
+//             arrCoordinates.push(coordinateMove)
+//         }
+//     }
+//     console.log(arrCoordinates);
+// }
+
+const getToTarget = (currentPos, dst) => {
+
+    const stack = [ currentPos ];
+
+    while (stack.length > 0) {
+        if (currentPos === dst) return currentPos;
+        currentPos = stack.pop();
+        for (let pair of pairs) {
+            const xPosition = currentPos[0] + pair[0];
+            const yPosition = currentPos[1] + pair[1];
+            const coordinateMove = [xPosition, yPosition];
+
+            if (coordinateMove[0] >= 0 && coordinateMove[0] < 8 && 
+                coordinateMove[1] >= 0 && coordinateMove[1] < 8) {
+                stack.push(coordinateMove)
+            }
         }
     }
-    console.log(arrCoordinates)
-    // console.log(arrCoordinates); //returns array of actual coordinates of the potential move
-
 }
+// getMoves([7,1]);
 
-getMoves([1,3]);
-const checkPosition = (currentMove) => {
-    //function that takes in our current move
-    //checks whether that move is valid on the board layout, we don't want moves going off the board
-
-
-}
+getToTarget([7,1], [3,3])
