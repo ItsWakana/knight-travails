c5666f5cb4ebceacf04d3774a7bbbf21c4104290
MultiFlex <wakanawardrobe@gmail.com>
2022-11-24 02:59:47 +0000

    try to fix loop of stack with visited Set

--- main.js
+++ main.js
@@ -32,25 +32,31 @@ const pairs = [[-1,-2], [-2,-1], [-2,1], [-1,2], [+1,2], [2,1], [2,-1], [1,-2]];
 //     console.log(arrCoordinates);
 // }
 
-const getToTarget = (currentPos, dst) => {
+const getToTarget = (currentPos, target) => {
 
     const stack = [ currentPos ];
+    const visited = new Set();
 
     while (stack.length > 0) {
-        if (currentPos === dst) return currentPos;
+        console.log(currentPos);
+        if (currentPos[0] === target[0] && currentPos[1] ===  target[1]) {
+            return currentPos;
+        }
+
         currentPos = stack.pop();
         for (let pair of pairs) {
             const xPosition = currentPos[0] + pair[0];
             const yPosition = currentPos[1] + pair[1];
             const coordinateMove = [xPosition, yPosition];
-
+            
             if (coordinateMove[0] >= 0 && coordinateMove[0] < 8 && 
                 coordinateMove[1] >= 0 && coordinateMove[1] < 8) {
-                stack.push(coordinateMove)
+                if (visited.has(coordinateMove)) continue;
+                stack.push(coordinateMove);
+                visited.add(coordinateMove);
             }
         }
     }
 }
-// getMoves([7,1]);
 
-getToTarget([7,1], [3,3])
+getToTarget([7,1], [3,3]);
