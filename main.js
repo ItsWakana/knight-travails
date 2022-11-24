ecfa94296a9c4ab5894d68fe208df687ef7cfc37
wakana <wakanawardrobe@gmail.com>
2022-11-24 17:49:35 +0000

    change move coordinates to object instead of array

--- main.js
+++ main.js
@@ -33,30 +33,53 @@ const pairs = [[-1,-2], [-2,-1], [-2,1], [-1,2], [+1,2], [2,1], [2,-1], [1,-2]];
 // }
 
 const getToTarget = (currentPos, target) => {
-
+    // const stack = [ currentPos ];
     const stack = [ currentPos ];
     const visited = new Set();
 
     while (stack.length > 0) {
-        console.log(currentPos);
-        if (currentPos[0] === target[0] && currentPos[1] ===  target[1]) {
-            return currentPos;
+        const current = stack.pop();
+        console.log(current);
+        if (visited.has(current)) continue;
+        if (current.x === target.x && current.y ===  target.y) {
+            console.log(current);
+            return current;
         }
 
-        currentPos = stack.pop();
         for (let pair of pairs) {
-            const xPosition = currentPos[0] + pair[0];
-            const yPosition = currentPos[1] + pair[1];
-            const coordinateMove = [xPosition, yPosition];
+            const xPosition = current.x + pair[0];
+            const yPosition = current.y + pair[1];
+            const move = { x: xPosition, y: yPosition };
             
-            if (coordinateMove[0] >= 0 && coordinateMove[0] < 8 && 
-                coordinateMove[1] >= 0 && coordinateMove[1] < 8) {
-                if (visited.has(coordinateMove)) continue;
-                stack.push(coordinateMove);
-                visited.add(coordinateMove);
+            // if (visited.has(move)) continue;
+            // if (coordinateMove[0] >= 0 && coordinateMove[0] < 8 && 
+            //     coordinateMove[1] >= 0 && coordinateMove[1] < 8) {
+
+            //     if (!visited.has(coordinateMove)) {
+            //         visited.add(coordinateMove);
+            //         stack.push(coordinateMove);
+            //     }
+            // }
+
+            if (move.x >= 0 && move.x < 8 && 
+                move.y >= 0 && move.y < 8) {
+
+                visited.add(move);
+                stack.push(move);
             }
         }
     }
 }
 
-getToTarget([7,1], [3,3]);
+const start = {
+    x: 7,
+    y: 1
+}
+
+const dst = {
+    x: 3,
+    y: 3
+}
+
+// getToTarget([7,1], [3,3]);
+getToTarget(start, dst);
