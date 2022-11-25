227a2df65f1030f23b2399df7a95d21a086de0db
wakana <wakanawardrobe@gmail.com>
2022-11-25 12:29:15 +0000

    fix traversal to destination

--- main.js
+++ main.js
@@ -13,35 +13,17 @@ chessboard = [
 
 const possibleMoves = [[-1,-2], [-2,-1], [-2,1], [-1,2], [1,2], [2,1], [2,-1], [1,-2]];
 
-// const getMoves = (currentPos) => {
-//     //from our array of coordinates, take in our current position and get all the positions of our array from the current position.
-//     const arrCoordinates = [];
-
-//     for (let pair of pairs) {
-//         //go through our pairs and get the correct indexes for our new position
-//         const xPosition = currentPos[0] + pair[0];
-//         const yPosition = currentPos[1] + pair[1];
-//         const coordinateMove = [xPosition, yPosition];
-
-//         //write some check here to make sure the move is actually valid
-//         if (coordinateMove[0] >= 0 && coordinateMove[0] < 8 && 
-//             coordinateMove[1] >= 0 && coordinateMove[1] < 8) {
-//             arrCoordinates.push(coordinateMove)
-//         }
-//     }
-//     console.log(arrCoordinates);
-// }
-
 const getToTarget = (currentPos, target) => {
     const stack = [ currentPos ];
     const visited = new Set();
 
     while (stack.length > 0) {
         const current = stack.pop();
-        // console.log(current);
-        if (visited.has(current)) continue;
+        const currentString = `${current.x},${current.y}`;
+        // if (visited.has(currentString)) continue;
+        console.log(currentString);
+        visited.add(currentString);
         if (current.x === target.x && current.y ===  target.y) {
-            console.log(current);
             return current;
         }
 
@@ -49,22 +31,15 @@ const getToTarget = (currentPos, target) => {
             const xPosition = current.x + child[0];
             const yPosition = current.y + child[1];
             const move = { x: xPosition, y: yPosition };
-            
-            // if (visited.has(move)) continue;
-            // if (coordinateMove[0] >= 0 && coordinateMove[0] < 8 && 
-            //     coordinateMove[1] >= 0 && coordinateMove[1] < 8) {
+            const moveToString = `${move.x},${move.y}`;
 
-            //     if (!visited.has(coordinateMove)) {
-            //         visited.add(coordinateMove);
-            //         stack.push(coordinateMove);
-            //     }
-            // }
 
             if (move.x >= 0 && move.x < 8 && 
                 move.y >= 0 && move.y < 8) {
-                console.log(move);
-                visited.add(move);
-                stack.push(move);
+                // visited.add(moveToString);
+                if (!visited.has(moveToString)) {
+                    stack.push(move);
+                }
             }
         }
     }
