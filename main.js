5d12b1277332aae7b0fe371a4a03d0a2f631bcf3
wakana <wakanawardrobe@gmail.com>
2022-11-28 15:05:32 +0000

    create backtrack object to make sure we get fastest route

--- main.js
+++ main.js
@@ -1,32 +1,23 @@
-const knight = 'k';
-
-chessboard = [
-    ['','','','','','','',''],
-    ['','','','','','','',''],
-    ['','','','','','','',''],
-    ['','','','','','','',''],
-    ['','','','','','','',''],
-    ['','','','','','','',''],
-    ['','','','','','','',''],
-    ['','','','','','','',''],
-]
-
 const possibleMoves = [[-1,-2], [-2,-1], [-2,1], [-1,2], [1,2], [2,1], [2,-1], [1,-2]];
 
 const getToTarget = (currentPos, target) => {
     const queue = [ currentPos ];
     const visited = new Set();
-    
+    const backtrackObj = {};
     let counter = -1;
     while (queue.length > 0) {
         const current = queue.shift();
         const currentString = `${current.x},${current.y}`;
-        console.log(currentString);
+
+        if (Object.keys(backtrackObj).length === 0) {
+        backtrackObj[currentString] = null;
+        }
         counter++;
         visited.add(currentString);
         if (current.x === target.x && current.y ===  target.y) {
-            console.log(`Reached ${currentString} in ${counter} moves`);
-            return current;
+            // console.log(`Reached ${currentString} in ${counter} moves`);
+            backtrackOrder(backtrackObj, currentPos, currentString);
+            return;
         }
 
         for (let child of possibleMoves) {
@@ -39,6 +30,7 @@ const getToTarget = (currentPos, target) => {
             if (move.x >= 0 && move.x < 8 && 
                 move.y >= 0 && move.y < 8) {
                 if (!visited.has(moveToString)) {
+                    backtrackObj[moveToString] = currentString;
                     queue.push(move);
                 }
             }
@@ -46,14 +38,28 @@ const getToTarget = (currentPos, target) => {
     }
 }
 
+const backtrackOrder = (obj, origin, current) => {
+    const fastestOrder = [];
+    const originString = `${origin.x},${origin.y}`;
+
+    while (obj[current] != null) {
+        fastestOrder.push(current);
+
+        current = obj[current];
+    }
+
+    fastestOrder.push(current);
+    console.log(fastestOrder);
+}
+
 const start = {
-    x: 7,
-    y: 1
+    x: 0,
+    y: 0
 }
 
 const destination = {
-    x: 5,
-    y: 0
+    x: 3,
+    y: 3
 }
 
 // getToTarget([7,1], [3,3]);
