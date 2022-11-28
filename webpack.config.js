96d18a411303b0c099b0fc903ad2f8962f8c4000
wakana <wakanawardrobe@gmail.com>
2022-11-28 16:22:02 +0000

    add webpack for ui

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: '[name][ext]',
+    },
+    devTool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ]
+    }
+}
